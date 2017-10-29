359001e83316c9584ecda1e6dfcb0e4da1b910d1
chasesmith <chasesmith@berkeley.edu>
2017-10-29 14:33:13 -0700

    Structured the contract

--- contracts/DKGRandomness.sol
+++ contracts/DKGRandomness.sol
@@ -14,24 +14,52 @@ It needs to:
 
 contract DKG {
 	mapping (address => address) public_keys;
+	//public key
+	//private key finally
 	mapping (address => bool) addresses;
-	mapping (address => stuff) check;
+	//mapping (address => stuff) check;
 	uint limit;
-	uint polynomial;
 	bool is_verified;
+	//could also stake the nodes
+
 	//list of things
 
 
 
 	function DKG() {
-		//owner?
-
+		//created with a prime ordering
+		//created with a generator
+		//created with an initial limit (number of participants)
+		//created with a degree (t)
 	}
 
-	function check();
+	/*
+		Add an individual node
+		requires a public key, c0, c1, .....ct-1
+	*/
+	function addNode();
 
 
-	function addAddress();
-	//Only add an address if it can do specific things
+	/*
+		Request node to distribute shared secrets to nodes
+	*/
+	function distributeKeys();
+
+
+	 /*
+		Individual Node verifies the validity of a key from another
+		node
+	*/
+	function verifyKey();
+
+	function acceptNode();
+
+	function getRandom();
+
+	function requestPrivateKey();
+
+	function verifyPrivateKeyPublicKeyPair();
+
+	function pieceTogetherPrivateKey();
 
 }
