17741f9747fec42eb974fb9302d2be1b8ce3218e
Shivane Sabharwal <shivanesabharwal@airbears2-10-142-10-63.airbears2.1918.berkeley.edu>
2017-10-29 15:59:47 -0700

    random poly started

--- js-src/polynomialoperator.js
+++ js-src/polynomialoperator.js
@@ -1,8 +1,11 @@
 
 
+<script src="mathlib.js" type="text/javascript">
+</script>
 
-
-function randomPolynomial(degree, secret_key) {};
+function randomPolynomial(degree, secret_key) {
+	
+};
 
 //function getsharedsecrets() {};
 //How does the random polynomial work.
