c2e2cf3fc37f7b84917ce99b6660e37ebf87a34d
Shivane Sabharwal <shivanesabharwal@airbears2-10-142-10-63.airbears2.1918.berkeley.edu>
2017-10-29 18:51:35 -0700

    Finished polynomial generation and interpolation

--- js-src/polynomialoperator.js
+++ js-src/polynomialoperator.js
@@ -1,10 +1,48 @@
 
+var algebra = require('algebra.js');
 
-<script src="mathlib.js" type="text/javascript">
-</script>
+/* Generates a random polynomial, P, of 
+degree specified where P(0) = secret_key  */
+function randomPolynomial(degree, secret_key, prime) {
+	var poly = [];
+	for (i = 0; i < degree; i++) {
+		poly[i] = getRandomInt(0, prime);
+	}
+	poly[degree] = secret_key;
+	return poly;
+};
 
-function randomPolynomial(degree, secret_key) {
-	
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+};
+
+
+/* Takes in an array of tuples, where each is a tuple of
+X and Y values, where Y = P(X). */
+function interpolation(points) {
+	var delta_polynomials = [];
+	for (int i = 0; i < points.length; i++) {
+		// constructing a delta polynomial for each point
+		var expr = new Expression(1);
+		var divide = 1;
+		for (int j = 0; j < points.length; j++) {
+			if (i != j) {
+				var tmpExpr = new Expression("x");
+				tmpExpr = tmpExpr.subtract(points[j][1]);
+				divide = divide * (points[i][0] - points[j][0]); 
+				expr = expr.multiply(tmpExpr);
+			}
+		}
+		expr = expr.divide("x");
+		delta_polynomials[i] = expr.divide(divide);
+	}
+	var poly = delta_polynomials[0].multiply(points[k][1]);
+	for (int k = 1; k < delta_polynomials.length; k++) {
+		poly = poly.add(delta_polynomials[k]).multiply(points[k][1]);
+	}
+	return poly;
 };
 
 //function getsharedsecrets() {};
